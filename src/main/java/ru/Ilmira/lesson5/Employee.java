005bf0bf7a4f1805f9d480d628352c9bed354cd4
Ilmira82 <ilmira.82@inbox.ru>
2021-05-26 20:43:43 +0300

    Домашнее задание № 5

---
+++ src/main/java/ru/Ilmira/lesson5/Employee.java
@@ -0,0 +1,63 @@
+package ru.Ilmira.lesson5;
+
+public class Employee {
+    private String name;
+    private String post;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+
+    public Employee(String name, String post, String email, String phoneNumber, int salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    // Переопределила стандартый метод для отображении информации об объекте.
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", post='" + post + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    // Написала отдельный метод для демонстрации разных вариантов решения этой задачи.
+    public void printInfo() {
+        System.out.println("ФИО: " + name + ", Должность: " + post + ", email: " + email + ", Контакты: " + phoneNumber + ", Зарплата: " + salary + ", Возраст: " + age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
---
+++ src/main/java/ru/Ilmira/lesson5/Main.java
@@ -0,0 +1,21 @@
+package ru.Ilmira.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Ivanov Ivan", "Manager", "ivanov@mail.ru", "89991231223", 50000, 25);
+        employees[1] = new Employee("Petrov Petr", "Engineer", "petrov@mail.ru", "89991231111", 45000, 30);
+        employees[2] = new Employee("Serova Mariya ", "Accountant", "serova@mail.ru", "89991230000", 60000, 50);
+        employees[3] = new Employee("Vagapov Nikita", "Architect", "vagapov@mail.ru", "89991232222", 65000, 45);
+        employees[4] = new Employee("Belova liya", "HR", "liya@mail.ru", "89991112233", 35000, 23);
+
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getAge() > 40) {
+                System.out.println(employees[i]);
+                //    employees[i].printInfo(); // Другой способ вывода информации
+            }
+        }
+    }
+}
+
